d87d762ead28e57ce766f6df32bd5831bd258078
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-19 21:03:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if time.hour < 12
       resp.write "Good Morning!"
     else
-      resp.write "Good Morning!"
+      resp.write "Good Afternoon!"
     end
     resp.finish
   end
