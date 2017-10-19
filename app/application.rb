7b0bcc655e0ba8c46ee95f8f3972c3100037d653
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-19 20:56:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,20 @@
+
+
 class Application
 
   def call
     resp = Rack::Responses.new
 
     time = Time.new
+
+    if time > 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
+
+    resp.finish
+    
   end
-  
+
 end
